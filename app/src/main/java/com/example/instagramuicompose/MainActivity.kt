a00fe131ccbc6fb1ef1b24d2d597327847c1935b
Dhruv Limbachiya <dhruv.mail4work@gmail.com>
2021-09-03 10:56:22 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/instagramuicompose/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.instagramuicompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.instagramuicompose.screen.ProfileScreen
+import com.example.instagramuicompose.ui.theme.InstagramUIComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
+
